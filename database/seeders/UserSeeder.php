13ec1b53bb65e5bbc740a03e0cd586c676ca4cd8
Ardhillah Habibi Alfath <ardhillahalfath@gmail.com>
2023-11-08 21:19:56 +0700

    Membuat seeder untuk user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,25 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $users = [
+            [
+                'name' => 'Robby',
+                'email' => 'robby@gmail.com',
+                'idrole' => 1,
+                'password' => Hash::make('admin123')
+            ], [
+                'name' => 'Alex',
+                'email' => 'alex@gmail.com',
+                'idrole' => 3,
+                'password' => Hash::make('admin123')
+            ], [
+                'name' => 'Ratna',
+                'email' => 'ratna@gmail.com',
+                'idrole' => 2,
+                'password' => Hash::make('admin123')
+            ],
+        ];
 
+        DB::table('users')->insert($users);
     }
 }
